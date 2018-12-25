e223313fb2142d36a1b7c7778a4b32a29af6e35d
fisker <lionkay@gmail.com>
2018-12-25 14:32:25 +0800

    move to seperate project

--- packages/eslint-config/rules/no-semi.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  rules: {
-    semi: ['error', 'never'],
-    'no-extra-semi': 'off',
-  }
-}
--- packages/eslint-config/rules/semi.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  rules: {
-    semi: ['error', 'always'],
-    'no-extra-semi': 'error'
-  }
-}
